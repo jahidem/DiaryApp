ee716c50ef258971c33562e010150d158fe78452
Jahid Hossain <54525656+jahidem@users.noreply.github.com>
2021-08-20 01:35:50 +0600

    added function to clear right panel

--- DiaryApp/DiaryApp.cpp
+++ DiaryApp/DiaryApp.cpp
@@ -4,52 +4,24 @@
 #include <iostream>
 #include<windows.h>
 #include<conio.h>
-#include "EntryInDiary.h"
-#include "AllEntrySingleton.h"
+#include "EntryInDiary.h"   // this represnts single entry on diary
+#include "AllEntrySingleton.h"  // Holds all the entryInDiary remains 
+         //throughout the liftime of the application
 #include <map>
 
-void color(int color) {
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
-
-void gotoxy(int x,int y) {
-    COORD c;
-    c.X = x;
-    c.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-void ShowConsoleCursor(bool visible) {
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 100;
-    info.bVisible = visible;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
-}
-void setFontSize() {
-    CONSOLE_FONT_INFOEX cfi;
-    cfi.cbSize = sizeof(cfi);
-    cfi.nFont = 0;
-    cfi.dwFontSize.X = 0;                   // Width of each character in the font
-    cfi.dwFontSize.Y = 20;                  // Height
-    cfi.FontFamily = FF_DONTCARE;
-    cfi.FontWeight = FW_BOLD;
-   // std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
-    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
-
-    //std::cout << "Font: Consolas, Size: 18\n";
-}
-void setConsoleDimen() {
-    HWND console = GetConsoleWindow();
-    RECT r;
-    GetWindowRect(console, &r); //stores the console's current dimensions
-
-    MoveWindow(console, r.left, r.top, 800, 700, TRUE); // 800 width, 100 height
-}
+void color(int color);  // just picks a color for output on terminal
+void gotoxy(int x, int y);  // moves the cursor to the position
+void ShowConsoleCursor(bool visible);   // to show or hide console cursor
+void setFontSize(); // set the font family,size.weight etc
+void setConsoleDimen(); // set the height and width of console
 using namespace std;
-int set[] = { 12,7,7,7 };
-char counter = 0;
-char key;
-void initialScreen();
-void showListEntry();
+int set[] = { 12,7,7,7 };   // just some numbers for colors of the Menu
+char counter = 0;   // indicates which item of the Menu is active
+char key;   // takes keyboard inputs
+
+void initialScreen();   // draws the initial UI
+void showListEntry();   // shows list on the right panel
+void clearRightPanel(); // clear al content int the right panel 
 int main()
 {  
    
@@ -82,18 +54,22 @@ void showListEntry() {
     while (true) {
         int nwGo = listCounter,move;
         map<int, int> mp;
+  
         for (move = 0,nwGo=listCounter; move < 30&&move<4*SZ; move += 4,nwGo++) {
             int colorCode = 8; nwGo %= SZ; mp[nwGo] = 1;
-            //for (int u = 2; u < 4&& nowShowing == nwGo; u++) {
-               // gotoxy(43,move+u); for (int v = 43; v <75; v++) cout << ' ';
-           // }
+            for (int u = 2; u < 4; u++) {//works fine for refreshing but casuses gliching
+                //need to be fixed
+                gotoxy(43, move + u); for (int v = 43; v < 75; v++) cout << ' ';
+            }
+        
+
             if (nowShowing == nwGo) colorCode = 12;
             color(colorCode); gotoxy(42, move + 1);
             cout << "\xDA";
             for (int u = 0; u < 32; u++) cout << '\xC4';
             cout << '\xBF';
             gotoxy(42, move + 2); cout << '\xBA', color(7);
-            cout << "Date: /  /  ID: " << lis[nwGo].getmTitle();
+            cout << "Date: /  /             ID: 000" << lis[nwGo].getmId();
 
 
             color(colorCode), gotoxy(42, move + 3); cout << '\xBA', color(7);
@@ -112,7 +88,9 @@ void showListEntry() {
         char keyList = _getch();
         if (keyList == 72) nowShowing--;
         else if (keyList == 80) nowShowing++;
-        else if(keyList==27) break;
+        else if (keyList == 27) {
+            clearRightPanel(); break;
+        }
         nowShowing +=SZ, nowShowing %= SZ;
         if (!mp[nowShowing]) {
             int tm;
@@ -158,4 +136,46 @@ void initialScreen() {
     color(set[3]);
     cout << "Exit";
     
+}
+void color(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void gotoxy(int x, int y) {
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void ShowConsoleCursor(bool visible) {
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = visible;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+void setFontSize() {
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = 20;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_BOLD;
+    // std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
+    //std::cout << "Font: Consolas, Size: 18\n";
+}
+void setConsoleDimen() {
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r); //stores the console's current dimensions
+
+    MoveWindow(console, r.left, r.top, 800, 720, TRUE); // 800 width, 100 height
+}
+void clearRightPanel() {
+    for (int u = 1; u <= 32; u++) {
+        for (int v = 42; v <= 75; v++) gotoxy(v, u), cout << ' ';
+    }
 }
